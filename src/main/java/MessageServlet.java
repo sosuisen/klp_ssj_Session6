f9a50ad1a1314098c74fdb62e827b8414dce4aae
sosuisen <hidekazu.kubota@gmail.com>
2023-04-30 18:17:48 +0900

    fix: use var

--- src/main/java/MessageServlet.java
+++ src/main/java/MessageServlet.java
@@ -6,13 +6,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = { "/message", "/clear" })
 public class MessageServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		HttpSession session = req.getSession();
+		var session = req.getSession();
 
 		if (session.getAttribute("history") == null) {
 			// セッションスコープにArrayListが保存されていなければ保存
@@ -33,10 +32,10 @@ public class MessageServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
-		String message = req.getParameter("message");
+		var message = req.getParameter("message");
 
-		HttpSession session = req.getSession();
-		ArrayList<String> list = (ArrayList<String>) session.getAttribute("history");
+		var session = req.getSession();
+		var list = (ArrayList<String>) session.getAttribute("history");
 		list.add(message);
 
 		req.getRequestDispatcher("/WEB-INF/message.jsp").forward(req, res);
