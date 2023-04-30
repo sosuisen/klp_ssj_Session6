c82d40b07af709b584767b840bfe13ff9e0b8ed6
sosuisen <hidekazu.kubota@gmail.com>
2023-04-30 14:03:12 +0900

    first commit

---
+++ src/main/java/MessageServlet.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = { "/message" })
+public class MessageServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		if (session.getAttribute("history") == null) {
+			// セッションスコープにArrayListが保存されていなければ保存
+			session.setAttribute("history", new ArrayList<String>());
+		}
+		req.getRequestDispatcher("/WEB-INF/message.jsp").forward(req, res);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String message = req.getParameter("message");
+		HttpSession session = req.getSession();
+		ArrayList<String> list = (ArrayList<String>) session.getAttribute("history");
+		list.add(message);
+		req.getRequestDispatcher("/WEB-INF/message.jsp").forward(req, resp);
+	}
+}
---
+++ src/main/webapp/WEB-INF/message.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="java.util.ArrayList"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="message" method="POST">
+		メッセージ入力 <input type="text" name="message">
+		<button>送信</button>
+	</form>
+	<hr>
+	<h1>メッセージ一覧</h1>
+	<br>
+	<%
+	ArrayList<String> list = (ArrayList<String>) session.getAttribute("history");
+	for (String mes : list) {
+	%>
+	<%=mes%><br>
+	<%
+	}
+	%>
+</body>
+</html>
