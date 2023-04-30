0597cd8608f64374b311835109b66f00a7bc9727
sosuisen <hidekazu.kubota@gmail.com>
2023-04-30 18:24:14 +0900

    fix: add model folder

---
+++ src/main/java/model/MessageBean.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageBean implements Serializable {
+	private String name;
+	private String message;
+}
